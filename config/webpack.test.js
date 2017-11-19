113228c649ebf94cd3669f67d5df1ac52f54caec
Andrey <skpuralsk@yandex.ru>
2017-11-19 22:33:52 +0300

    fix webpack warning

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -44,7 +44,7 @@ module.exports = {
     plugins: [
         new webpack.ContextReplacementPlugin(
             // The (\\|\/) piece accounts for path separators in *nix and Windows
-            /angular(\|\/)core(\|\/)@angular/,
+            /angular(\\|\/)core(\\|\/)@angular/,
             helpers.root('./src'), // location of your src
             {} // a map of your routes
         )
